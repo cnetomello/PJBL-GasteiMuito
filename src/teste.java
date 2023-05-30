d504af1d9f288bb35d09e8080757c7d2916e7e10
Murilo Baduy <baduymurilo@gmail.com>
2023-05-30 20:05:25 -0300

    separacao de codigo

--- src/teste.java
+++ src/teste.java
@@ -67,8 +67,8 @@ public class teste extends JFrame {
                         System.out.println(infos);
 
                         // Criar e exibir a próxima tela
-                        TelaSeguinte telaSeguinte = new TelaSeguinte(nome, renda_fl);
-                        telaSeguinte.exibir();
+                        Aba2 aba2 = new Aba2(nome, renda_fl);
+                        Aba2.exibir();
 
                         // Fechar a tela atual
                         frame.dispose();
@@ -80,71 +80,5 @@ public class teste extends JFrame {
                 }
             }
         });
-    }
-}
-
-class TelaSeguinte extends JFrame {
-    private String nome;
-    private float renda;
-
-    // Campos extras
-    private JLabel despesaLabel;
-    private JTextField inputDespesa;
-    private JLabel gastoLabel;
-    private JTextField inputGasto;
-    private JLabel categoriaLabel;
-    private JComboBox<String> categoriaComboBox;
-
-    public TelaSeguinte(String nome, float renda) {
-        this.nome = nome;
-        this.renda = renda;
-
-        // Inicialize os componentes extras
-        despesaLabel = new JLabel("Tipo de Despesa:");
-        inputDespesa = new JTextField();
-        gastoLabel = new JLabel("Valor Gasto:");
-        inputGasto = new JTextField();
-        categoriaLabel = new JLabel("Categoria:");
-        String[] categorias = {"Alimentação", "Transporte", "Lazer", "Saúde", "Outros"};
-        categoriaComboBox = new JComboBox<>(categorias);
-    }
-
-    public void exibir() {
-        // Criação da janela da tela seguinte
-        JFrame frame = new JFrame("Tela Seguinte");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.setLocationRelativeTo(null);
-
-        // Criação dos componentes da tela seguinte
-        JLabel label = new JLabel("Nome: " + nome + ", Renda: " + renda);
-        label.setFont(new Font("Arial", Font.BOLD, 20));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setBounds(50, 100, 300, 50);
-
-        // Posicionamento dos componentes extras
-        despesaLabel.setBounds(50, 150, 150, 30);
-        inputDespesa.setBounds(50, 190, 300, 30);
-        gastoLabel.setBounds(50, 230, 100, 30);
-        inputGasto.setBounds(50, 270, 300, 30);
-        categoriaLabel.setBounds(50, 310, 100, 30);
-        categoriaComboBox.setBounds(50, 350, 300, 30);
-
-        // Adicione os componentes extras ao painel
-        JPanel telaSeguinte = new JPanel();
-        telaSeguinte.setLayout(null); // Layout vertical
-        telaSeguinte.add(label);
-        telaSeguinte.add(despesaLabel);
-        telaSeguinte.add(inputDespesa);
-        telaSeguinte.add(gastoLabel);
-        telaSeguinte.add(inputGasto);
-        telaSeguinte.add(categoriaLabel);
-        telaSeguinte.add(categoriaComboBox);
-
-        // Adiciona o componente à janela
-        frame.getContentPane().add(telaSeguinte);
-
-        // Exibe a janelax
-        frame.setVisible(true);
     }
 }
