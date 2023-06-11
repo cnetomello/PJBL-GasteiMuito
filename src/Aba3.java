a2fca70c842871ccde8b6bf972a0e2bc2344e6bf
Cyril Zakhia <cyrilzakhia@hotmail.com>
2023-06-11 00:33:50 -0300

    Exibir Total Gastos etc...

--- src/Aba3.java
+++ src/Aba3.java
@@ -1,4 +1,4 @@
-import javax.swing.*;
+/*import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,6 +7,7 @@ public class Aba3 extends tela {
     private ArrayList<ArrayList<Object>> informacoes;
     private String nome;
     private float renda;
+    private float total;
 
     public Aba3(ArrayList<ArrayList<Object>> informacoes , String nome , float renda){
         this.informacoes = informacoes;
@@ -16,7 +17,7 @@ public class Aba3 extends tela {
     // [0]: Nome despesa , [1]: Categoria , [2]: Valor Gasto , [3] : Data_despesa
 
     public void exibir(){
-
+        total =0;
         String [] colunas = new String[informacoes.size()+1];
         colunas[0]="Despesas/Data";
         for (int i=0 ; i< informacoes.size();i++){
@@ -24,6 +25,7 @@ public class Aba3 extends tela {
 
 
             colunas[i+1]= informacoes.get(i).get(3)+ "";
+            total +=  Float.parseFloat( informacoes.get(i).get(2)+"");
 
         }
         colunas= Arrays.stream(colunas).distinct().toArray(String[]::new);
@@ -62,12 +64,15 @@ public class Aba3 extends tela {
             }
         System.out.println();}
 
+        JLabel Total = new JLabel("TOTAL GASTOS " + total);
+
 
         JTable table_despesa = new JTable(dados,colunas);
 
         JFrame frame = new JFrame("GasteiMuito");
 
         frame.add(new JScrollPane(table_despesa));
+        frame.add(Total);
         frame.setSize(700,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -77,6 +82,90 @@ public class Aba3 extends tela {
 
 
 
+
+    }
+}
+*/
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.text.DecimalFormat;
+
+public class Aba3 extends tela {
+    private ArrayList<ArrayList<Object>> informacoes;
+    private String nome;
+    private float renda;
+    private float total;
+
+    public Aba3(ArrayList<ArrayList<Object>> informacoes, String nome, float renda) {
+        this.informacoes = informacoes;
+        this.nome = nome;
+        this.renda = renda;
+    }
+
+    // [0]: Nome despesa, [1]: Categoria, [2]: Valor Gasto, [3]: Data_despesa
+
+    public void exibir() {
+        total = 0;
+        String[] colunas = new String[informacoes.size() + 1];
+        colunas[0] = "Despesas/Data";
+        for (int i = 0; i < informacoes.size(); i++) {
+            colunas[i + 1] = informacoes.get(i).get(3) + "";
+            total += Float.parseFloat(informacoes.get(i).get(2) + "");
+        }
+        colunas = Arrays.stream(colunas).distinct().toArray(String[]::new);
+
+        Object[][] dados = new String[informacoes.size()][colunas.length];
+        for (int i = 0; i < dados.length; i++) {
+            String[] c = new String[colunas.length];
+            for (int j = 0; j < colunas.length; j++) {
+                if (j == 0) {
+                    c[j] = informacoes.get(i).get(0) + "";
+                    continue;
+                } else if (colunas[j].equals(informacoes.get(i).get(3))) {
+                    c[j] = informacoes.get(i).get(2) + "";
+                } else {
+                    c[j] = "";
+                }
+                System.out.println("Infos" + informacoes.get(i).get(3));
+                System.out.println("Coluna" + colunas[j]);
+                System.out.println(colunas[j].equals(informacoes.get(i).get(3)));
+            }
+            dados[i] = c;
+        }
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String total_round = decimalFormat.format(total);
+        String renda_round = decimalFormat.format(renda);
+        String saldo = decimalFormat.format(renda-total);
+        JLabel Total = new JLabel("   TOTAL GASTOS " + total_round);
+        JLabel Saldo = new JLabel("                                         Saldo : R$ "+ saldo);
+        JLabel nome_siu = new JLabel("Nome: " + nome + "                                 ");
+        JLabel renda_siu = new JLabel("Renda : R$ " + renda_round);
+        JPanel container = new JPanel(new BorderLayout());
+        JPanel container_end = new JPanel(new BorderLayout());
+        container.setSize(200,100);
+
+        JTable table_despesa = new JTable(dados, colunas);
+
+
+        JPanel panel = new JPanel(new BorderLayout());
+        container_end.add(Total, BorderLayout.LINE_START);
+        container_end.add(Saldo, BorderLayout.CENTER);
+        panel.add(container_end , BorderLayout.PAGE_END);
+        container.add(nome_siu, BorderLayout.LINE_START);
+        container.add(renda_siu,BorderLayout.CENTER);
+        panel.add(container ,BorderLayout.PAGE_START);
+        panel.add(new JScrollPane(table_despesa), BorderLayout.CENTER);
+
+
+        JFrame frame = new JFrame("GasteiMuito");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main (String [] args){
-        Aba1 aba1 = new Aba1();
-        aba1.exibir();
-      /*  ArrayList<ArrayList<Object>> test =  new ArrayList<ArrayList<Object>>();
+    public static void main (String [] args) {
+         Aba1 aba1 = new Aba1();
+         aba1.exibir(); 
+    /*    ArrayList<ArrayList<Object>> test = new ArrayList<ArrayList<Object>>();
         ArrayList<Object> siu = new ArrayList<>();
         siu.add("Coucou");
         siu.add("Alimentacao");
@@ -12,12 +12,12 @@ public class Main {
         siu.add("20/05/2023");
         test.add(siu);
         ArrayList<Object> miu = new ArrayList<>();
-        miu.add("Miaou" );
+        miu.add("Miaou");
         miu.add("Alimentacao");
         miu.add("10.05");
         miu.add("21/05/2023");
         test.add(miu);
-Aba3 aba3 = new Aba3(test ,"Cyril" , 40.05f);
-aba3.exibir(); */
+        Aba3 aba3 = new Aba3(test, "Cyril", 40.05f);
+        aba3.exibir(); */
     }
 }
